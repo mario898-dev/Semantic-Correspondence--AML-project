3e267d365534f1e157f350081a37d69769f61d2e
Tommaso Centonze <tcentonze01@gmail.com>
2026-01-02 19:36:44 +0100

    Update backbone choices in CLI argument parser

--- project_utils/cli.py
+++ project_utils/cli.py
@@ -7,7 +7,7 @@ def parse_eval_args():
         "--backbone",
         type=str,
         required=True,
-        choices=["dinov2_vitb14", "dinov2_vitl14", "dinov3_vits16","dinov3_vitb16", "dinov3_vitl16", "sam_vitb"],
+        choices=["dinov2_vits14", "dinov2_vitb14", "dinov2_vitl14", "dinov3_vits16","dinov3_vitb16", "dinov3_vitl16", "sam_vitb"],
     )
 
     parser.add_argument(
