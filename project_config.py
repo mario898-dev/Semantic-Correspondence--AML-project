9f703349fe579510838f1a66f62c405cf243ef6a
Tommaso Centonze <tcentonze01@gmail.com>
2026-01-07 20:13:10 +0100

    Increase image size from 518 to 1024

--- project_config.py
+++ project_config.py
@@ -6,7 +6,7 @@ class Config:
         # Percorso dove viene scaricato SPair-71k 
         ROOT = os.path.join(os.getcwd(), 'dataset')
         
-        IMG_SIZE = 518
+        IMG_SIZE = 1024
         
         MEAN = False
         NORM_MEAN = [0.485, 0.456, 0.406]
