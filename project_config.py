58bb4dc2854f140daccfcb0e2a840b78c6179919
Mario <70803088+mario898-dev@users.noreply.github.com>
2026-01-07 01:04:18 +0100

    change directory dataset

--- project_config.py
+++ project_config.py
@@ -1,14 +1,18 @@
 import os
 
+REPO_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 class Config:
     class DATASET:
         NAME = 'spair'
         # Percorso dove viene scaricato SPair-71k 
         ROOT = os.path.join(os.getcwd(), 'dataset')
         
-        IMG_SIZE = 320
+        IMG_SIZE = 518
         
-        MEAN = [0.485, 0.456, 0.406]
+        MEAN = False
+        NORM_MEAN = [0.485, 0.456, 0.406]
+        NORM_STD = [0.229, 0.224, 0.225]
         STD = [0.229, 0.224, 0.225]
 
     class EVALUATOR:
