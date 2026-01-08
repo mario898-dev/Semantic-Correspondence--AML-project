a11b9d0bdc775b7afeb0f12b74ba99a8bf74bd50
Tommaso Centonze <tcentonze01@gmail.com>
2026-01-08 17:17:24 +0100

    Update project_config.py

--- project_config.py
+++ project_config.py
@@ -6,7 +6,7 @@ class Config:
         # Percorso dove viene scaricato SPair-71k 
         ROOT = os.path.join(os.getcwd(), 'dataset')
         
-        IMG_SIZE = 518
+        IMG_SIZE = 512
         
         MEAN = [0.485, 0.456, 0.406]
         STD = [0.229, 0.224, 0.225]
