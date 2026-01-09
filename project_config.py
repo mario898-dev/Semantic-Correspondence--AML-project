8178614f962c5ceb4ac45ea2e5dc0d9d7dce99f0
Mario <70803088+mario898-dev@users.noreply.github.com>
2026-01-09 02:55:24 +0100

    size 224 per dinov2

--- project_config.py
+++ project_config.py
@@ -6,7 +6,7 @@ class Config:
         # Percorso dove viene scaricato SPair-71k 
         ROOT = os.path.join(os.getcwd(), 'dataset')
         
-        IMG_SIZE = 1024
+        IMG_SIZE = 224
         
         MEAN = [0.485, 0.456, 0.406]
         STD = [0.229, 0.224, 0.225]
