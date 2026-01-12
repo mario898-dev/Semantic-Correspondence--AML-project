c5df3ffa2cea06c64e86fbc8bbf80219bea3e016
s339356 <s339356@studenti.polito.it>
2026-01-12 19:32:47 +0100

    Refactor eval.py to include additional datasets

--- eval.py
+++ eval.py
@@ -7,6 +7,8 @@ import wandb
 
 from utils.cli import parse_eval_args
 from dataset.spair import SPairDataset
+from dataset.pfpascal import PFPascalDataset
+from dataset.pfwillow import PFWillowDataset
 from utils.matching import find_correspondences
 from utils.metrics import compute_pck_metrics
 from project_config import Config
@@ -222,6 +224,7 @@ def run_evaluation(args):
 if __name__ == "__main__":
     args = parse_eval_args()
     run_evaluation(args)
+
 
 
 
